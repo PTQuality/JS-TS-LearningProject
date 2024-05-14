dbe7eb73b3f4b8113013af435ae51ea0c701a5cb
PTQuality <kontakt.tunskipiotr@gmail.com>
2024-05-14 18:05:36 +0200

    Third and last coding module.
    - Next step is TS course
    - After TS course finally will start course for Playwright for E2E tets

--- js-base/exercises/S03/3.file.operations.js
+++ js-base/exercises/S03/3.file.operations.js
@@ -4,31 +4,23 @@
 // 3. Write code to read content of created file "ex3.txt"
 // 4. Display content of that file on console.
 
-
 // to test your solution in terminal You can run following command:
 // npm run es3e3
 
 //// TODO:
 // here place your solution:
 
+import { writeFileSync, readFileSync } from "fs";
 
+const dataToFile = "Hello jaktestowac.pl";
+const filePath = "exercises/S03/ex3.txt";
+writeFileSync(filePath, dataToFile);
 
-
-
-
-
-
-
-
-
-
-
-
-
+const dataFromFile = readFileSync(filePath, { encoding: "utf8" });
+console.log(dataFromFile);
 
 //// DONT MODIFY CODE BELOW!
 // Here You will find expected result of exercise and verification!
-
 
 // Expected output:
 // Hello jaktestowac.pl!
